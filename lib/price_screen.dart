0ae484af73147f2613da4e7867dd14115ce141ad
Rahimi1531 <naiab1531@gmail.com>
2023-06-21 12:42:57 -0700

    first commit

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,8 +9,31 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  List<DropdownMenItems> getDropdownItems() {
+    List<DropdwonMenuItems<String>> DropdownItems = [];
+    for (String currency in currenciesList) {
+      String currency = currenciesList[i];
+      var newItem = DropdownMenuItems(
+        child: Text(currency),
+        value: currency,
+      );
+    }
+    DropdownItems.add(newItems);
+    return DropdownItems;
+  }
+
+  List<Text> getPickerItems() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      Text(currency);
+    }
+    return pickerItems;
+  }
+
   @override
   Widget build(BuildContext context) {
+    getDropdownItems();
     return Scaffold(
       appBar: AppBar(
         title: const Text('🤑 Coin Ticker'),
@@ -38,12 +64,16 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: CupertinoPicker(
+                BackgroundColor: Colors.LightBlue,
+                ItemExtent: 32.5,
+                onselectedItemChange: (selectedIndex) {},
+                childern: getPickerItems(),
+              )),
         ],
       ),
     );
